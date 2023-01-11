d35f36337c632d23ee5191e32887b6ab55639161
Daniil Vlasenko <daniil.vlasenko@paradoxcat.com>
2023-01-11 15:54:27 +0100

    latest experiments

--- cpp2variant/cpp2variant.cpp
+++ cpp2variant/cpp2variant.cpp
@@ -1,13 +1,31 @@
 // ----- Cpp2 support -----
+#define CPP2_USE_SOURCE_LOCATION Yes
 #include "cpp2util.h"
 
-
 #line 1 "cpp2variant.cpp2"
+#include <vector>
+
+struct EngineObject
+{
+    EngineObject() :val(std::rand()) {}
+    int val;
+    auto operator<=>(const EngineObject&) const = default;
+};
+
+struct PropertyValue { std::variant<bool, int, double, std::vector<PropertyValue>, std::shared_ptr<EngineObject>> val_; };
+
+[[nodiscard]] auto compare(cpp2::in<PropertyValue> lhs, cpp2::in<PropertyValue> rhs) -> std::partial_ordering;
+#line 16 "cpp2variant.cpp2"
 [[nodiscard]] auto main() -> int;
 
 //=== Cpp2 definitions ==========================================================
 
-#line 1 "cpp2variant.cpp2"
+#line 11 "cpp2variant.cpp2"
+
+[[nodiscard]] auto compare(cpp2::in<PropertyValue> lhs, cpp2::in<PropertyValue> rhs) -> std::partial_ordering{
+    return std::partial_ordering::unordered; 
+}
+
 [[nodiscard]] auto main() -> int{
     std::cout << "hello world!";
 }
